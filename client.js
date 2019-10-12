d3062f7c4df1e8a26784a74a5f7ed651be021ade
Zarwlar <barbuhs2@gmail.com>
2019-10-12 12:12:58 +0700

    Add client like interactive

--- client.js
+++ client.js
@@ -1,3 +1,13 @@
 var node = document.getElementById('slider-root');
+var addItemBtn = document.getElementById('add-item-btn');
+var itemNameInput = document.getElementById('item-name-input');
+var itemValueInput = document.getElementById('item-value-input');
+
 const slider = new Slider(node);
-slider.addItem('props')
+
+addItemBtn.addEventListener('click', function () {
+  var name = itemNameInput.value;
+  var value = Number.parseInt(itemNameInput.value) || undefined;
+
+  slider.addItem(name, value);
+})
